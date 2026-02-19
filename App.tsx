3388c2d97f4a409538f02ed9b28b669e20b491d3
Maria Eduarda Caixeta <mariaeduarda.caixeta@performait.com>
2026-02-19 11:24:50 -0300

    route config

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import "./src/shared/styles/global.css";
 import { Login } from './src/screens/Login';
+import NavigationRoutes from '@/routes';
 
 export default function App() {
   return (
-    <Login />
+    <NavigationRoutes />
   );
 }
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,9 +1,20 @@
-import { Text, View } from "react-native"
+import { PublicStackParamsList } from "@/routes"
+import { useNavigation } from "@react-navigation/native"
+import { StackNavigationProp } from "node_modules/@react-navigation/stack/lib/typescript/src/types"
+import { Text, TouchableOpacity, View } from "react-native"
 
 export const Login = () => {
+
+    const navigation = useNavigation<StackNavigationProp<PublicStackParamsList>>()
+
     return (
         <View className="flex-1 items-center justify-center">
             <Text>Login Screen</Text>
+            <TouchableOpacity
+                onPress={() => navigation.navigate("Register")}
+            >
+                <Text>Registrar</Text>
+            </TouchableOpacity>
         </View>
     )
 }
