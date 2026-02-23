6da279b0ebef9fdf603474651bd9b12dc00adc5e
Maria Eduarda Caixeta <mariaeduarda.caixeta@performait.com>
2026-02-23 14:05:52 -0300

    finalizing login screen

--- src/screens/Login/LoginForm/index.tsx
+++ src/screens/Login/LoginForm/index.tsx
@@ -1,41 +1,50 @@
-import { AppButton } from "@/components/AppButton"
-import { AppInput } from "@/components/AppInput"
-import { useForm } from "react-hook-form"
-import { Text } from "react-native"
+import { AppButton } from "@/components/AppButton";
+import { AppInput } from "@/components/AppInput";
+import { useForm } from "react-hook-form";
+import { Text, View } from "react-native";
 
 export interface LoginFormParams {
-    email: string
-    password: string
+  email: string;
+  password: string;
 }
 
 export function LoginForm() {
-    const {
-        control,
-        handleSubmit,
-        formState: { isSubmitting },
-    } = useForm<LoginFormParams>()
+  const {
+    control,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<LoginFormParams>();
 
-    return (
-        <>
-            <AppInput
-                control={control}
-                name="email"
-                label="EMAIL"
-                leftIconName="mail-outline"
-                placeholder="Digite seu email"
-            />
-            <AppInput
-                control={control}
-                name="password"
-                label="SENHA"
-                placeholder="Sua senha"
-                leftIconName="lock-outline"
-                secureTextEntry
-            />
+  return (
+    <>
+      <AppInput
+        control={control}
+        name="email"
+        label="EMAIL"
+        leftIconName="mail-outline"
+        placeholder="Digite seu email"
+      />
+      <AppInput
+        control={control}
+        name="password"
+        label="SENHA"
+        placeholder="Sua senha"
+        leftIconName="lock-outline"
+        secureTextEntry
+      />
 
-            <AppButton mode="outline" iconName="arrow-forward">
-                <Text>Login</Text>
-            </AppButton>
-        </>
-    )
+      <View className="flex-1 justify-between mt-8 mb-6 min-h-[250px]">
+        <AppButton iconName="arrow-forward">
+          <Text>Login</Text>
+        </AppButton>
+
+        <View>
+            <Text className="mb-6 text-gray-300 text-base">Ainda não possui uma conta?</Text>
+          <AppButton mode="outline" iconName="arrow-forward">
+            <Text>Cadastrar</Text>
+          </AppButton>
+        </View>
+      </View>
+    </>
+  );
 }
