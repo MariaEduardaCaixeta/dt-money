8bc6f96c95cd5775c6601f4f445de699bea407c2
Maria Eduarda Caixeta <mariaeduarda.caixeta@performait.com>
2026-02-23 12:05:30 -0300

    adding logo image

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -4,11 +4,13 @@ import { useNavigation } from "@react-navigation/native"
 import { StackNavigationProp } from "node_modules/@react-navigation/stack/lib/typescript/src/types"
 import { Text, TextInput, TouchableOpacity, View } from "react-native"
 import { LoginForm } from "./LoginForm"
+import { AuthHeader } from "@/components/AuthHeader"
 
 export const Login = () => {
     return (
         <DismissKeyboardView>
             <View className="flex-1 w-[82%] self-center">
+                <AuthHeader />
                 <LoginForm />
             </View>
         </DismissKeyboardView>
