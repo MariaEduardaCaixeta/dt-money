c0a58746d425184d74aad6031323b580b60f2708
Maria Eduarda Caixeta <mariaeduarda.caixeta@performait.com>
2026-02-24 10:44:55 -0300

    consuming user info from context api

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,17 +3,18 @@ import { PublicRoutes } from './PublicRoutes'
 import { useCallback, useState } from 'react'
 import { PrivateRoutes } from './PrivateRoutes';
 import { StatusBar } from 'expo-status-bar';
+import { useAuthContext } from '@/context/auth.context';
 
 const NavigationRoutes = () => {
-    const [user, setUser] = useState(undefined)
+    const { user, token } = useAuthContext();
 
     const Routes = useCallback(() => {
-        if (user) {
+        if (user || token) {
             return <PrivateRoutes />
         }
 
         return <PublicRoutes />
-    }, [user])
+    }, [user, token])
 
     return (
         <NavigationContainer>
