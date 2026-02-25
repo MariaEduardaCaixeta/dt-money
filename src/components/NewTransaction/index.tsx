efc6c89e1ae1a1e27b5b3246b52e6ace2d573945
Maria Eduarda Caixeta <mariaeduarda.caixeta@performait.com>
2026-02-25 15:57:29 -0300

    adding description and value inputs

--- src/components/NewTransaction/index.tsx
+++ src/components/NewTransaction/index.tsx
@@ -3,7 +3,8 @@ import { colors } from "@/shared/colors";
 import { ICreateTransactionRequest } from "@/shared/interfaces/create-transaction-request";
 import { MaterialIcons } from "@expo/vector-icons";
 import { useState } from "react";
-import { TouchableOpacity, View, Text } from "react-native";
+import { TouchableOpacity, View, Text, TextInput } from "react-native";
+import CurrencyInput from "react-native-currency-input";
 
 export function NewTransaction() {
   const { closeBottomSheet } = useBottomSheetContext();
@@ -15,6 +16,16 @@ export function NewTransaction() {
     value: 0,
   });
 
+  const setTransactionData = (
+    key: keyof ICreateTransactionRequest,
+    value: string | number,
+  ) => {
+    setTransaction((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  };
+
   return (
     <View className="px-8 py-5">
       <TouchableOpacity
@@ -24,6 +35,27 @@ export function NewTransaction() {
         <Text className="text-white text-xl font-bold">Nova transação</Text>
         <MaterialIcons name="close" size={20} color={colors.gray[700]} />
       </TouchableOpacity>
+
+      <View className="flex-1 mt-8 mb-8">
+        <TextInput
+          className="text-white text-lg h-[50px] bg-background-primary my-2 rounded-[6] pl-4"
+          placeholder="Descrição"
+          placeholderTextColor={colors.gray[700]}
+          value={transaction.description}
+          onChangeText={(text) => setTransactionData("description", text)}
+        />
+
+        <CurrencyInput 
+            className="text-white text-lg h-[50px] bg-background-primary my-2 rounded-[6] pl-4"
+            value={transaction.value}
+            onChangeValue={(value) => setTransactionData("value", value ?? 0)}
+            prefix="R$ "
+            delimiter="."
+            separator=","
+            minValue={0}
+            precision={2}
+        />
+      </View>
     </View>
   );
 }
