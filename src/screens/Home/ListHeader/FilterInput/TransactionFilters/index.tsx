66eb595687e48cd286ad82a170b103c7b8400e30
Maria Eduarda Caixeta <mariaeduarda.caixeta@performait.com>
2026-02-26 12:43:20 -0300

    adding transaction type filter

--- src/screens/Home/ListHeader/FilterInput/TransactionFilters/index.tsx
+++ src/screens/Home/ListHeader/FilterInput/TransactionFilters/index.tsx
@@ -4,6 +4,7 @@ import { MaterialIcons } from "@expo/vector-icons";
 import { Text, TouchableOpacity, View } from "react-native";
 import { DateFilter } from "./DateFilter";
 import { CategoryFilter } from "./CategoryFilter";
+import { TypeFilter } from "./TypeFilter";
 
 export const TransactionFilters = () => {
   const { closeBottomSheet } = useBottomSheetContext();
@@ -21,6 +22,8 @@ export const TransactionFilters = () => {
       <DateFilter />
 
       <CategoryFilter />
+
+      <TypeFilter />
     </View>
   );
 };
